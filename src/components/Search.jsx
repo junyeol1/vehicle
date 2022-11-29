f05d9f4a3af24ca0376644b74dc3ebe802c03961
YooKyubin <binde73@naver.com>
2022-11-29 22:51:46 +0900

    이거 왜  미리 커밋 안했지

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,23 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import styled from "styled-components";
 
 function Search(props){
 
-    const data = props.keywords;
+    const [data, setData] = useState([]);
     const getURL = props.parentFunction;
+    const [URL, setURL] = useState('http://localhost:3001/board');
+
+    useEffect(()=>{
+        // 전체 게시글 정보를 검색 컴포넌트로 넘기기 위해
+        // select 문을 변결할 때마다 검색 select에 나오는 옵션을 알맞게 변경시켜주기 위해선 이부분 수정이 필요함
+        fetch(URL) 
+        .then(res=>{
+            return res.json();
+        })
+        .then(data=>{
+            setData(data);
+        })
+    },[URL])
 
     var startProvince  = new Set();
     var arrivalProvince = new Set();
@@ -47,10 +60,19 @@ function Search(props){
         apRef.current.value="";
         acRef.current.value="";
         getURL('http://localhost:3001/board');
+        setURL('http://localhost:3001/board');
     }
 
     const selectChange = (e) =>{
         // select가 변결될 때마다 옵션 항목을 변경하기 위해 여기에 뭘 작성해야 할걱타음
+        e.preventDefault();
+        var url = 'http://localhost:3001/board?';
+        url += !spRef.current.value ? "": `startProvince=${spRef.current.value}&`;
+        url += !scRef.current.value ? "": `startCity=${scRef.current.value}&`;
+        url += !apRef.current.value ? "": `arrivalProvince=${apRef.current.value}&`;
+        url += !acRef.current.value ? "": `arrivalCity=${acRef.current.value}&`;
+        setURL(url);
+
     }
 
     return <>
@@ -65,7 +87,7 @@ function Search(props){
                     </option>
                 })}
             </select>
-            <select ref={scRef} name="sc">
+            <select ref={scRef} name="sc" onClick={()=>console.log(1111)} onChange={selectChange}>
                 <option value={""}>선택 안함</option>
                 {setSC.map(sc => {
                     return <option key={sc} value={sc}>
@@ -77,7 +99,7 @@ function Search(props){
             <br />
 
             <span>목적지</span>
-            <select ref={apRef} name="ap">
+            <select ref={apRef} name="ap" onChange={selectChange}>
                 <option value={""}>선택 안함</option>
                 {setAP.map(ap => {
                     return <option key={ap} value={ap}>
@@ -85,7 +107,7 @@ function Search(props){
                     </option>
                 })}
             </select>
-            <select ref={acRef} name='ac'>
+            <select ref={acRef} name='ac' onChange={selectChange}>
                 <option value={""}>선택 안함</option>
                 {setAC.map(ac => {
                     return <option key={ac} value={ac}>
--- src/pages/CarpoolInfo.jsx
+++ src/pages/CarpoolInfo.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function CarpoolInfo(){
-    const id = useParams().id;
-    return <h1>{id} 게시글</h1>;
+
+    const boardId = useParams().id;
+    // 게시글 정보
+    const [info, setInfo] = useState([]);
+
+    // 동승자 정보
+    const [passengerId, setPassengerId] = useState([]);
+
+    // 게시글 권한 정보
+    const userId = localStorage.getItem['idx'];
+
+    useEffect(()=>{        
+        // 게시글 정보 받아오기
+        fetch(`http://localhost:3001/board?id=${boardId}`)
+        .then(res=>{
+            return res.json();
+        })
+        .then(data=>{
+            setInfo(data[0]);
+        })
+    },[])
+
+    return <>
+        <h3>게시글 정보:</h3>
+        <h4>{info.title}</h4>
+        <h4>{info.id}</h4>
+        <h4>{info.start}</h4>
+        <h4>{info.arrival}</h4>
+    </>;
 }
 
 export default CarpoolInfo;
